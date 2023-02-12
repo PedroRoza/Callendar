2d5f596ae6144be9d1f772222b2f51653bcd8e4f
Pedro Vinícius <pedrosilva.rosa@hotmail.com>
2023-02-12 17:31:35 -0300

    config server to front end

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
 
 const app = Fastify()
+const prisma = new PrismaClient()
 /**
  * Methods HTTP: Get (search), Post(create), Put(update), Patch(especific update), Delete
  */
 
-app.get('/', () => {
-  return 'hello world'
+app.register(cors)
+
+app.get('/home', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
 })
 
 app.listen({
